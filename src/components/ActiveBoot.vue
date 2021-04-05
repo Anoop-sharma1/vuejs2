5d370a2c765b28a3ad1df393fb8dded3951e8720
Ankur Tiwari <ankur.tiwari@gyrix.in>
2021-04-05 11:14:15 +0530

    Completed till lec 23 add bootstrap

---
+++ src/components/ActiveBoot.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>Active Boot</h1>
+
+        <b-row>
+    <b-col md="auto">
+      <b-calendar v-model="value" @context="onContext" locale="en-US"></b-calendar>
+    </b-col>
+    <b-col>
+
+      <pre class="small">{{ context }}</pre>
+    </b-col>
+  </b-row>
+    </div>
+</template>
+
+<script>
+export default {
+    name : 'ActiveBoot'
+}
+</script>
